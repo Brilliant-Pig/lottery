8c04cd2d59a547e34b52ddd28426dd4f5a320944
Brilliant-Pig <181056374+Brilliant-Pig@users.noreply.github.com>
2025-02-26 00:21:40 +0800

    Initial commit: project setup

---
+++ lottery/src/router/router.js
@@ -0,0 +1,75 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+    {
+        path: '/',
+        name: '',
+        redirect: '/demo'
+    },
+    {
+        path: '/login-main',
+        name: 'login-main',
+        component: () => import('../pages/login-main.vue'),
+        meta: {
+            title: '登录页面',
+            freeAuth: true
+        }
+    },
+    {
+        path: '/lottery-main',
+        name: 'lottery-main',
+        component: () => import('../pages/lottery-main.vue'),
+        meta: {
+            title: '抽奖首页',
+            freeAuth: true
+        }
+    },
+    {
+        path: '/customers-main',
+        name: 'customers-main',
+        component: () => import('../pages/customers-main.vue'),
+        meta: {
+            title: '参与抽奖页面',
+            freeAuth: true
+        }
+    },
+    {
+        path: '/managers-main',
+        name: 'managers-main',
+        component: () => import('../pages/managers-main.vue'),
+        meta: {
+            title: '管理人页面',
+            freeAuth: true
+        }
+    },
+    {
+        path: '/result-main',
+        name: 'result-main',
+        component: () => import('../pages/result-main.vue'),
+        meta: {
+            title: '抽奖结果展示',
+            freeAuth: true
+        }
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+// 导航守卫
+router.beforeEach((to, from, next) => {
+    // 从 localStorage 中读取 token
+    const token = localStorage.getItem('token');
+    // 判断当前页面 meta 信息是否包含免认证标识
+    const freeAuthMeta = to.matched.some((record) => record.meta.freeAuth);
+    // 如果不是免认证的页面并且没有token，则跳转到登录页
+    if (!freeAuthMeta && !token) {
+        next({ name: 'login' });
+    } else {
+        next();
+    }
+});
+
+export default router;
---
+++ lottery/vite.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// vite.config.ts
+import autoImportPlugin from '@opentiny/unplugin-tiny-vue'
+import { TinyVueResolver } from '@opentiny/unplugin-tiny-vue'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    autoImportPlugin('vite'),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver(), TinyVueResolver],
+    }),
+  ],
+  define: {
+    'process.env': { ...process.env }
+  },
+  configureWebpack: {
+    plugins: [
+      Components({
+        resolvers: [TinyVueResolver]
+      }),
+      AutoImport({
+        resolvers: [TinyVueResolver]
+      })
+    ]
+  }
+})
+
+
