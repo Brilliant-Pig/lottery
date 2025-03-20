698a104caf2af1e5b2b9823d286e77ca80f2ee5c
Brilliant-Pig <181056374+Brilliant-Pig@users.noreply.github.com>
2025-03-21 01:07:18 +0800

    fix-拨乱反正

--- lottery/src/main.js
+++ lottery/src/main.js
@@ -1,13 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import store from './store/index';
 import router from './router/router' ;// 修正路径
 import './style.css';
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 const app = createApp(App)
-app.use(store);
 app.use(router);
 app.use(ElementPlus)
 app.mount('#app')
