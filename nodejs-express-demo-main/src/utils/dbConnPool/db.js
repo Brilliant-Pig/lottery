3e2097f9d89ab8a4edf9c67f338636d929af583a
Brilliant-Pig <181056374+Brilliant-Pig@users.noreply.github.com>
2025-03-28 14:43:14 +0800

    docs-实现插入和修改（必须拉取）

--- nodejs-express-demo-main/src/utils/dbConnPool/db.js
+++ nodejs-express-demo-main/src/utils/dbConnPool/db.js
@@ -3,6 +3,6 @@ const db_conf = require('config').get('dbConfig');
 // 根据数据库类型导入对应的CURD操作
 const provider = db_conf.provider;
 
-const db = require(`./${provider}`);
+const db = require('./' + provider);
 
 module.exports = db;
