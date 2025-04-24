c052e38f629267f8825d86ee0129c47758c7b709
Sesiy <181816593+Sesiy@users.noreply.github.com>
2025-04-25 01:33:01 +0800

    docs

--- lottery/src/router/router.js
+++ lottery/src/router/router.js
@@ -100,6 +100,15 @@ const routes = [
             freeAuth: true
         }
     },
+    {
+        path: '/TotalAnimation',
+        name: 'TotalAnimation',
+        component: () => import('../pages/TotalAnimation.vue'),
+        meta: { 
+            title: '管理员动画页面',
+            freeAuth: true
+        }
+    }
 ];
 
 const router = createRouter({
