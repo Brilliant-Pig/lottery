0d04d0b2c92e53e1ab3eddf40fb8790597091434
Brilliant-Pig <181056374+Brilliant-Pig@users.noreply.github.com>
2025-05-12 00:51:28 +0800

    fix-mainpages页面大字

--- lottery/src/main.js
+++ lottery/src/main.js
@@ -11,6 +11,13 @@ import { ElMessage } from 'element-plus';
 import { createPinia } from 'pinia';
 
 const app = createApp(App);
+// 存储原始 body 高度
+const originalBodyHeight = document.body.style.height;
+
+// 在每次路由跳转后还原 body 高度
+router.afterEach(() => {
+    document.body.style.height = originalBodyHeight;
+});
 app.use(createPinia())
 app.use(router);
 app.use(store);
--- lottery/src/pages/mainpages.vue
+++ lottery/src/pages/mainpages.vue
@@ -165,6 +165,11 @@
         <span>联系客服</span>
         </div>
     </footer>
+    <router-view v-slot="{ Component }">
+        <keep-alive>
+            <component :is="Component" />
+        </keep-alive>
+        </router-view>
     </div>
 </template>
 
@@ -174,6 +179,9 @@ import axios from 'axios';
 const DEFAULT_AVATAR = 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png';
 
 export default {
+    activated() {
+        this.loadUserData(); // 每次进入页面时刷新数据
+    },
     created(){
         const username = localStorage.getItem('username');
         if (username) {
@@ -250,6 +258,7 @@ export default {
         return statusMap[status] || status || '未知状态';},
     viewDetail(id) {
         console.log('查看详情:', id)
+        this.$router.push('/ResultMan')
     }, 
     switchMainTab(tab) {
         // 处理底部导航切换逻辑
